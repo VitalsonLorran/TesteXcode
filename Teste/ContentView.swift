a7912c63a8c85ef28dd0386ac3bb6659d8b5e715
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2021-09-20 19:34:24 -0300

    Initial Commit

---
+++ Teste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Teste
+//
+//  Created by Vitalson Lorran on 20/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Teste/TesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteApp.swift
+//  Teste
+//
+//  Created by Vitalson Lorran on 20/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
